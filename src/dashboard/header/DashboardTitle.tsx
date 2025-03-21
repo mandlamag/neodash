0f0425beff2b960c77fda3fecb5740c2d116bdda
Mandla <mandla.magagula@gmail.com>
2025-03-21 10:15:36 +0200

    update label

--- src/dashboard/header/DashboardTitle.tsx
+++ src/dashboard/header/DashboardTitle.tsx
@@ -63,7 +63,7 @@ export const NeoDashboardTitle = ({
   }
 
   useEffect(() => {
-    document.title = dashboardTitle ? `NeoDash - ${dashboardTitle}` : 'NeoDash - Neo4j Dashboard Builder';
+    document.title = dashboardTitle ? `LedgerLink - ${dashboardTitle}` : 'LedgerLink - Dashboard Builder';
     // Reset text to the dashboard state when the page gets reorganized.
     if (dashboardTitle !== dashboardTitleText) {
       setDashboardTitleText(dashboardTitle);
--- src/dashboard/placeholder/DashboardPlaceholder.tsx
+++ src/dashboard/placeholder/DashboardPlaceholder.tsx
@@ -12,7 +12,7 @@ export const NeoDashboardPlaceholder = () => {
             <div className='n-flex n-justify-between n-h-16 n-items-center n-py-6 md:n-justify-start md:n-space-x-10 n-mx-4'>
               <NeoDashboardHeaderLogo />
               <nav className='n-items-center n-justify-center n-flex n-flex-1 n-w-full'>
-                NeoDash <BoltIconSolid className='icon-base' color='gold' />
+                LedgerLink <BoltIconSolid className='icon-base' color='gold' />
               </nav>
               <div className='sm:n-flex n-items-center n-justify-end md:n-flex-1 lg:n-w-0 n-gap-6'></div>
             </div>
